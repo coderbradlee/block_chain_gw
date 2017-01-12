375f09061a4bb5cf38331da2bb63b53bdde0de01
lzxm160 <lzxm160@sina.com>
2017-01-12 17:56:52 +0800

    btc

--- src/README.md
+++ src/README.md
@@ -2,3 +2,69 @@
 2、调用ethereum的http rpc接口
 3、实现两者互通转账
 4、实现智能合约存储比特币的信息
+
+
+用bitcoin-testnet-box
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 help
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 sendtoaddress "n1bnrtuqShki6wUSUVHRJ6dvc3EH46H38a" 12
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getinfo
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getaccountaddress ""
+output::mkxoF3Vqa3D2EtBPEYbhsZgXfpKuoHyqP2
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19011 -rpcuser=admin2 -rpcpassword=123 getaccountaddress ""
+output::n1Dgewo57eFPcRRPanSwzcAdYbLQ8saH8S
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 sendtoaddress "n1bnrtuqShki6wUSUVHRJ6dvc3EH46H38a" 12
+output::3e3b7baf784266230691a0939fcb8409766e82a0e79bca20b1dc29f38c3840d7
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getaccount mkxoF3Vqa3D2EtBPEYbhsZgXfpKuoHyqP2 
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress n1bnrtuqShki6wUSUVHRJ6dvc3EH46H38a
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 gettransaction 3e3b7baf784266230691a0939fcb8409766e82a0e79bca20b1dc29f38c3840d7
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getbalance n1bnrtuqShki6wUSUVHRJ6dvc3EH46H38a
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 generate 100
+
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 sendtoaddress "n1bnrtuqShki6wUSUVHRJ6dvc3EH46H38a" 1000
+output::fdfaf5f1f925c75d40e9094cfe7f4ed011a3fb23ed43117fb207eed691012d7e
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 sendtoaddress "mkxoF3Vqa3D2EtBPEYbhsZgXfpKuoHyqP2" 1000
+output::39a4eb3135f894ded592b45e3e89a95a9ead03e4b370bd8664bcaeda31747031
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getbalance mkxoF3Vqa3D2EtBPEYbhsZgXfpKuoHyqP2
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getmininginfo
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 listaddressgroupings
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19011 -rpcuser=admin2 -rpcpassword=123 listaddressgroupings
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 sendtoaddress "mzbnA97gw6L32bShQeNemS52ZhhCukTupS" 120 
+output::f32f0d6e1fd8b30cbdee2e6e15a6d1aa2de20a48eb677439b14d27c4bfd6ed41
+
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getbalance mzbnA97gw6L32bShQeNemS52ZhhCukTupS
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 listtransactions
+
+
+getaddressesbyaccount "" 获取钱包里面的地址
+getreceivedbyaddress 获取可以支配的余额
+
+/////至少6个确认块
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress mkxoF3Vqa3D2EtBPEYbhsZgXfpKuoHyqP2 6
+1000
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress myQoCs8vRqnBFAG36v4BtUGNQAzbLpJ6dF
+0
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress mu9ESBCd53C3z9H81YVCMMQcYmQUYHgLQE
+0
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress mzbnA97gw6L32bShQeNemS52ZhhCukTupS
+0
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19001 -rpcuser=admin1 -rpcpassword=123 getreceivedbyaddress n1Dgewo57eFPcRRPanSwzcAdYbLQ8saH8S
+0
+
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19011 -rpcuser=admin2 -rpcpassword=123 getreceivedbyaddress mzbnA97gw6L32bShQeNemS52ZhhCukTupS
+bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=19011 -rpcuser=admin2 -rpcpassword=123 getreceivedbyaddress n1Dgewo57eFPcRRPanSwzcAdYbLQ8saH8S
+
+以上两个地址是19011这个钱包里的，用19001来调用得到的是0
--- src/btc/main.go
+++ src/btc/main.go
@@ -2,23 +2,25 @@ package main
  
 import (
     "github.com/btcsuite/btcd/chaincfg"
+    "github.com/btcsuite/btcd/btcjson"
     "github.com/btcsuite/btcrpcclient"
     "github.com/btcsuite/btcutil"
     "log"
-    "net/http"
+    _"net/http"
     "os"
     "encoding/json"
-    "time"
-    "strings"
-    "io/ioutil"
+    _"time"
+    _"strings"
+    _"io/ioutil"
     "flag"
     "runtime"
+    "fmt"
 )
 type Configuration struct {
-    Ip string,
-    Port string,
-    User string,
-    Pass string,
+    Ip string
+    Port string
+    User string
+    Pass string
     Log_name string
 }
 var configuration Configuration
@@ -27,16 +29,21 @@ var configuration Configuration
 // )
 func init() {
     
-    file, _ := os.Open("conf.json")
+    file, _ := os.Open("src/btc/conf.json")
     decoder := json.NewDecoder(file)
     configuration = Configuration{}
     err := decoder.Decode(&configuration)
     if err != nil {
       fmt.Println("error:", err)
     }
-    log_init()
+    
     //fmt.Println(configuration.exec_time) // output: [UserA, UserB]
-    //fmt.Printf("%s\n",configuration.Exec_time)
+    fmt.Printf("%s\n",configuration.Ip)
+    fmt.Printf("%s\n",configuration.Port)
+    fmt.Printf("%s\n",configuration.User)
+    fmt.Printf("%s\n",configuration.Pass)
+    fmt.Printf("%s\n",configuration.Log_name)
+    log_init()
 }
 func log_init() {
     log_name:=fmt.Sprintf("%s",configuration.Log_name)
@@ -44,7 +51,6 @@ func log_init() {
     runtime.GOMAXPROCS(runtime.NumCPU())
     flag.Parse()
 
-    //set logfile Stdout
     logFile, logErr := os.OpenFile(*logFileName, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
     if logErr != nil {
         fmt.Println("Fail to find", *logFile, "cServer start Failed")
@@ -75,26 +81,82 @@ func main() {
         log.Printf("%s: %s", label, amount)
     }
  
-    // prepare a sendMany transaction
-    // receiver1, err := btcutil.DecodeAddress("1someAddressThatIsActuallyReal", &chaincfg.MainNetParams)
-    // if err != nil {
-    //     log.Fatalf("address receiver1 seems to be invalid: %v", err)
-    // }
-    // receiver2, err := btcutil.DecodeAddress("1anotherAddressThatsPrettyReal", &chaincfg.MainNetParams)
-    // if err != nil {
-    //     log.Fatalf("address receiver2 seems to be invalid: %v", err)
-    // }
-    // receivers := map[btcutil.Address]btcutil.Amount{
-    //     receiver1: 42,  // 42 satoshi
-    //     receiver2: 100, // 100 satoshi
-    // }
+    ///////////////////////////////////////////
+    blockCount, err := client.GetBlockCount()
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Printf("Block count: %d", blockCount)
+/**
+ * GetBalance 
+ n3f5v9CVLYaNN4RhCdYQp2xYZy1dpE6kA4
+n48tCmjZ4SnpzHbLDbZGnyaj57k4MbDZLz
+mhRPbdhG2jgu248xASxcE5WzYLAcVWR6Wi
+ */
+    address1:="n1h8unx5AqXcCJMzRELsKP1Af2RXcvubFu"
+    address2:="n1mh3d6m39oFY8ZCRdp8kGqJBeUC8HXC9A"
+    address3:="myQoCs8vRqnBFAG36v4BtUGNQAzbLpJ6dF"
+    // log.Printf("%T\n",client)
+    client_get_balance(address1,client)
+    client_get_balance(address2,client)
+    client_get_balance(address3,client)
+    //////prepare a sendMany transaction
+    receiver1, err := btcutil.DecodeAddress(address1, &chaincfg.MainNetParams)
+    if err != nil {
+        log.Fatalf("address receiver1 seems to be invalid: %v", err)
+    }
+    receiver2, err := btcutil.DecodeAddress(address2, &chaincfg.MainNetParams)
+    if err != nil {
+        log.Fatalf("address receiver2 seems to be invalid: %v", err)
+    }
+    receivers := map[btcutil.Address]btcutil.Amount{
+        receiver1: 100000000,  // 42 satoshi
+        receiver2: 100000000, // 100 satoshi
+    }
  
-    // // create and send the sendMany tx
-    // txSha, err := client.SendMany("some-account-label-from-which-to-send", receivers)
-    // if err != nil {
-    //     log.Fatalf("error sendMany: %v", err)
+    // err1:=client.WalletPassphrase("",60) 
+    // if err1 != nil {
+    //     log.Fatalf("error WalletPassphrase: %v", err1)
     // }
-    // log.Printf("sendMany completed! tx sha is: %s", txSha.String())
+    // create and send the sendMany tx
+    txSha, err := client.SendMany("", receivers)
+    if err != nil {
+        log.Fatalf("error sendMany: %v", err)
+    }
+    log.Printf("sendMany completed! tx sha is: %s", txSha.String())
+    //btcjson.GetTransactionResult
+    c,err:=client.GetTransaction(txSha)
+    if err != nil {
+        log.Fatalf("error GetTransaction: %v", err)
+    }
+    print(c)
+    //log.Printf("%f, %f, %d", c.Amount, c.Fee, c.Confirmations)
 }
 
 
+func print(c *btcjson.GetTransactionResult) {
+   
+    // BlockHash       string                        `json:"blockhash"`
+    // BlockIndex      int64                         `json:"blockindex"`
+    // BlockTime       int64                         `json:"blocktime"`
+    // TxID            string                        `json:"txid"`
+    // WalletConflicts []string                      `json:"walletconflicts"`
+    // Time            int64                         `json:"time"`
+    // TimeReceived    int64                         `json:"timereceived"`
+    // Details         []GetTransactionDetailsResult `json:"details"`
+    // Hex             string
+    
+    log.Printf("%f, %f, %d", c.Amount, c.Fee, c.Confirmations)
+}
+
+func client_get_balance(address string,c *btcrpcclient.Client) {
+    addr, err := btcutil.DecodeAddress(address, &chaincfg.MainNetParams)
+    if err != nil {
+        log.Fatalf("addr seems to be invalid: %v", err)
+    }
+    balance, err := c.GetReceivedByAddress(addr)
+    if err != nil {
+        log.Fatalf("address balance: %v", err)
+    }
+    log.Printf("address balance: %d", balance)
+}
